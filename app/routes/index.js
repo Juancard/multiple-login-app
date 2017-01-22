a9de9c106525b1a8d12b2c78d418c46d0bba299f
juan <juancruz.23@hotmail.com>
2017-01-22 18:56:53 -0300

    bug fix from unlink

--- app/routes/index.js
+++ app/routes/index.js
@@ -164,19 +164,15 @@ module.exports = function (app, passport) {
             failureRedirect : '/'
         }));
 
-		app.route('/unlink/:account$')
+		app.route('/unlink/:account(github|facebook|twitter|local|google)$')
 			.get(isLoggedIn, function(req, res){
-				var accountsAvailable = ["google", "twitter", "github", "facebook", "local"];
 				var account = req.params.account;
-				if (accountsAvailable.indexOf(account) > -1 && req.user.hasOwnProperty(account)){
-					var user = req.user;
-					user[account].state = user.unactiveState();
-					user.save(function(err){
-						if (err) throw err;
-						console.log("User has been correctly unlinked");
-						res.redirect("/");
-					});
-				}
-				res.redirect("/profile");
+				var user = req.user;
+				user[account].state = user.unactiveState();
+				user.save(function(err){
+					if (err) throw err;
+					console.log("User has been correctly unlinked");
+					res.redirect("/profile");
+				});
 			});
 };
