7eedb753b058f77cd534c5a35554a42715dd1b35
juan <juancruz.23@hotmail.com>
2017-01-24 16:17:39 -0300

    more refactoring: one verify callback for all types of account, implemented only in github

---
+++ app/config/authentication/strategies/CallbackVerificator.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var User = require('../../../models/users');
+
+var CallbackVerificator = function(provider, credentials){
+  this.provider = provider;
+}
+
+CallbackVerificator.prototype = {
+  fillUser: function(user, profile, token, callback){
+     user[this.provider].id = profile.id;
+     user[this.provider].token = token;
+     user[this.provider].username = profile.username;
+     user[this.provider].displayName = profile.displayName;
+     user[this.provider].state = user.activeState();
+     callback(null, user);
+   },
+   verifyCallback: function (req, token, refreshToken, profile, done) {
+
+     // Save in which this function gets called
+     // (when in nextTick, it changes and this.fillUser won't work anymore)
+     // (Guessed right, it's a patch)
+     var self = this;
+
+    /*
+    process.nextTick() is Node syntax that makes the code asynchronous.
+    Node will wait until the current "tick" of the event loop completes
+    before executing the callback function.
+    This essentially makes Node wait until the user information comes back
+    from GitHub before processing the results
+    */
+    process.nextTick(function () {
+
+      if (!req.user){
+        // if user is NOT already logged in
+        var providerId = profile.provider + ".id";
+        User.findOne({ providerId: profile.id }, function (err, user) {
+
+            if (err) {
+                return done(err);
+            }
+
+            if (user) {
+              if (user[profile.provider].state == user.unactiveState()) {
+                self.fillUser(user, profile, token, function(err, user){
+                  user.save(function(err) {
+                      if (err) throw err;
+                      return done(null, user);
+                  });
+                });
+              } else{
+                return done(null, user);
+              }
+            } else{
+              var user = new User();
+              user.nbrClicks.clicks = 0;
+              self.fillUser(user, profile, token, function(err, user){
+                user.save(function(err) {
+                    if (err) throw err;
+                    return done(null, user);
+                });
+              });
+            }
+        });
+      } else {
+        var user = req.user;
+        self.fillUser(user, profile, token, function(err, user){
+          user.save(function(err) {
+              if (err) throw err;
+              return done(null, user);
+          });
+        });
+      }
+    });
+  }
+}
+
+module.exports = CallbackVerificator;
--- app/config/authentication/strategies/github.js
+++ app/config/authentication/strategies/github.js
@@ -3,7 +3,10 @@
 var GitHubStrategy = require('passport-github').Strategy;
 var User = require('../../../models/users');
 var configAuth = require('../auth');
+var CallbackVerificator = require("./CallbackVerificator.js");
+
 var provider = "github";
+
 var credentials = {
   clientID: configAuth.githubAuth.clientID,
   clientSecret: configAuth.githubAuth.clientSecret,
@@ -12,72 +15,19 @@ var credentials = {
   scope: 'user:email'
 }
 
-function fillUser(user, profile, token, callback){
-  user[provider].id = profile.id;
-  user[provider].token = token;
-  user[provider].username = profile.username;
-  user[provider].displayName = profile.displayName;
-  user[provider].publicRepos = profile._json.public_repos;
-  user[provider].email = profile.emails[0].value;
-  user[provider].state = user.activeState();
-  callback(null, user);
+var fillUser = function(user, profile, token, callback){
+    user[provider].id = profile.id;
+    user[provider].token = token;
+    user[provider].username = profile.username;
+    user[provider].displayName = profile.displayName;
+    user[provider].publicRepos = profile._json.public_repos;
+    user[provider].email = profile.emails[0].value;
+    user[provider].state = user.activeState();
+    callback(null, user);
 }
-
-var verifyCallback = function (req, token, refreshToken, profile, done) {
-  /*
-  process.nextTick() is Node syntax that makes the code asynchronous.
-  Node will wait until the current "tick" of the event loop completes
-  before executing the callback function.
-  This essentially makes Node wait until the user information comes back
-  from GitHub before processing the results
-  */
-  process.nextTick(function () {
-    if (!req.user){
-      // if user is NOT already logged in
-      var providerId = provider + ".id";
-      User.findOne({ providerId: profile.id }, function (err, user) {
-
-          if (err) {
-              return done(err);
-          }
-
-          if (user) {
-            if (user[provider].state == user.unactiveState()) {
-              fillUser(user, profile, token, function(err, user){
-                user.save(function(err) {
-                    if (err) throw err;
-                    return done(null, user);
-                });
-              });
-            } else{
-              return done(null, user);
-            }
-          } else{
-            var user = new User();
-            user.nbrClicks.clicks = 0;
-            fillUser(user, profile, token, function(err, user){
-              user.save(function(err) {
-                  if (err) throw err;
-                  return done(null, user);
-              });
-            });
-          }
-      });
-    } else {
-      var user = req.user;
-      fillUser(user, profile, token, function(err, user){
-        user.save(function(err) {
-            if (err) throw err;
-            return done(null, user);
-        });
-      });
-    }
-  });
-}
-
 
 module.exports = function (passport) {
-
-  passport.use(new GitHubStrategy(credentials, verifyCallback));
-
+  var callbackVerificator = new CallbackVerificator(provider);
+  callbackVerificator.fillUser = fillUser;
+  passport.use(new GitHubStrategy(credentials, callbackVerificator.verifyCallback.bind(callbackVerificator)));
 }
